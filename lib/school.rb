c6b8fc6b77e574a962e3b30570aafea635da5ffb
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-05 19:52:10 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,17 @@
-# code Here
+require 'pry'
+
 class School
+  attr_accessor :school_name, :roster
 
-attr_accessor :name, :roster
+  def initialize(school_name)
+    @roster = {}
+  end
 
-def initialize(name)
-  @name = name
-  @roster = {}
-end
+  def add_student(name, grade)
+    @roster.has_key?(grade)
+    @roster[grade] = [name]
+  end
 
-def add_student(name, grade)
-  roster[grade] ||= []
-  roster[grade] << name
-end
 
-def grade(grade)
-  return roster[grade]
-end
 
-def sort
-  new_hash = {}
-  roster.each do |first_name, last_name|
-    new_hash[first_name] = last_name.sort
-end
-return new_hash
-end
 end
