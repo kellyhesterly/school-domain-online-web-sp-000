df684596c1503bf8118f249de871919f1d11008a
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-02-05 20:39:11 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -20,7 +20,7 @@ class School
 
   def sort
     # binding.pry
-    @roster.each do |grade|
+    @roster.each do |grade_x, grade_y|
       grade.map do |name_1, name_2|
         name_1 <=> name_2
         end
